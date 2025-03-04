d6178ad1f2f661c0f67152ef314b89d0b23ab392
Coutinhodacruz <dominicrotimi@gmail.com>
2025-03-04 11:53:43 +0100

    Changes made

--- src/component/FeatureSection.tsx
+++ src/component/FeatureSection.tsx
@@ -7,7 +7,7 @@ const FeaturesSection = () => {
           <h2 className="text-xl md:text-2xl font-bold text-blue-600 mb-6">
             Features of the Hacker House
           </h2>
-          <ul className="grid gap-3">
+          <ul className="grid gap-3 list-none pl-0">
             {[
               "Brainstorming and Ideation Sessions",
               "Workshops and Tutorials",
@@ -16,8 +16,8 @@ const FeaturesSection = () => {
               "Networking Opportunities",
               "Solution Demonstrations"
             ].map((text, index) => (
-              <li key={index} className="flex items-center gap-2">
-                <span className="text-blue-500 self-center">•</span>
+              <li key={index} className="flex items-start gap-2">
+                <span className="text-blue-500 self-start">•</span>
                 <span className="text-sm md:text-base">{text}</span>
               </li>
             ))}
@@ -25,13 +25,12 @@ const FeaturesSection = () => {
         </div>
       </div>
 
-  
       <div className="bg-[#f2f6fb] p-8 lg:p-16">
         <div className="max-w-3xl mx-auto">
           <h2 className="text-xl md:text-2xl font-bold text-blue-600 mb-6">
             Benefits for Participating Teams
           </h2>
-          <ul className="grid gap-3">
+          <ul className="grid gap-3 list-none pl-0">
             {[
               "Access Mentorship & Coaching",
               "Engage with the Sui Foundation",
@@ -41,8 +40,8 @@ const FeaturesSection = () => {
               "All participants who successfully complete the Hacker House are eligible for a reward worth the equivalent of $100",
               "Participants would build robust solutions using Sui and prepare for the Sui Overflow 2025 Hackathon, a virtual global competition with over $500,000 in combined prizes."
             ].map((text, index) => (
-              <li key={index} className="flex items-center gap-2">
-                <span className="text-blue-500 self-center">•</span>
+              <li key={index} className="flex items-start gap-2">
+                <span className="text-blue-500 self-start">•</span>
                 <span className="text-sm md:text-base">{text}</span>
               </li>
             ))}
